39890f2ddf16f52453a336ee229d817adf4e5886
Claes Östlund <claes@stenbergit.se>
2024-03-07 16:41:59 +0100

    1.0.0

---
+++ PluginShareFacebook.php
@@ -0,0 +1,26 @@
+<?php
+class PluginShareFacebook{
+  function __construct() {
+    wfPlugin::includeonce('wf/yml');
+  }
+  public function page_demo(){
+    wfPlugin::enable('share/facebook');
+    wfPlugin::enable('icons/bootstrap_v1_8_1');
+    $widget = wfDocument::createWidget('share/facebook', 'button_share_page');
+    wfDocument::renderElement(array($widget));
+  }
+  public function widget_button_share_page($data){
+    $data = new PluginWfArray($data);
+    if(!$data->get('data/u')){
+      if(wfRequest::get('u')){
+        $data->set('data/u', wfRequest::get('u'));
+      }else{
+        $data->set('data/u', wfServer::calcUrl(true));
+      }
+    }
+    $data->set('data/href', 'http://www.facebook.com/sharer/sharer.php?u='.$data->get('data/u'));
+    $element = wfDocument::getElementFromFolder(__DIR__, __FUNCTION__);
+    $element->setByTag($data->get('data'));
+    wfDocument::renderElement($element);
+  }
+}
